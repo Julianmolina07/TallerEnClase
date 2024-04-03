bff2f97615ef5596541588331494c602e7cdb4a1
Yeremy Daniel Silva Guerrero <145410510+yeremy0418@users.noreply.github.com>
2024-04-03 18:40:32 -0500

    Taller En Clase

---
+++ Taller-En-Clase/src/Persona.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Persona {
+    private String nombre;
+    private LocalDate fechaNacimiento;
+
+    public Persona(){
+        
+    }
+
+    public Persona(String nombre, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int[] calcularEdad() {
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    
+}
